6a51e2ed109ae5022e79f115f8eee3c79ae2a753
axel-krebs <axel.krebs@online.de>
2022-05-09 15:16:35 +0200

    initial-4
    - Added bundle fragment for Neo4J backend (preview)

---
+++ igs-core-backend-neo4j-fragment/src/main/java/eu/tecfox/cloud/identity/impl/neo4j/IdentityServiceImpl.java
@@ -0,0 +1,20 @@
+package eu.tecfox.cloud.identity.impl.neo4j;
+
+import eu.tecfox.cloud.identity.api.Identity;
+import eu.tecfox.cloud.identity.api.IdentityService;
+
+public class IdentityServiceImpl implements IdentityService {
+
+	@Override
+	public Identity find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasRealm() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
