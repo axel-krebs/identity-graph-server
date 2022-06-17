d55731fa4bce7497e8c7f29a7a2a3d8f2498ca11
axel-krebs <axel.krebs@online.de>
2022-06-17 16:07:43 +0200

    Test Case structure using bundles

---
+++ igs-mp-faulttolerance-bundle/src/main/java/de/akrebs/cloud/faulttolerance/impl/FTBundleActivator.java
@@ -0,0 +1,20 @@
+package de.akrebs.cloud.faulttolerance.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class FTBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ igs-tc-parent/igs-tc-search-bundle/src/main/java/de/akrebs/cloud/sample/search/impl/SearchBundleActivator.java
@@ -0,0 +1,20 @@
+package de.akrebs.cloud.sample.search.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class SearchBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
